53e7a939701fda7d6df620808346217176c5e2a6
Chen <xchen13@paypal.com>
2019-09-04 19:02:26 +0800

    1

--- vuemap/src/components/Map.vue
+++ vuemap/src/components/Map.vue
@@ -12,81 +12,90 @@
     <button class="startProcess"
             type="button"
             v-on:click="startProcess()">启动流程</button>
-    <el-form :model="ruleForm"
-             :rules="rules"
-             ref="ruleForm"
-             label-width="0px"
+    <el-form ref="ruleForm"
+             label-width="20px"
              class="demo-ruleForm">
       <el-form-item prop="delay_time">
-        <el-input v-model="ruleForm.dtime"
-                  class="input-color"
-                  placeholder="delay time"></el-input>
+        <el-input class="delaytime"
+                  placeholder="延误时间(单位小时)"
+                  v-model="dtime"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button class="aside-link"
+        <el-button class="submit"
                    @click="handleCommand()">提交</el-button>
       </el-form-item>
     </el-form>
-    <div v-show="shipshow"
-         class="info">
-      <table border="1">
-        <tr>
-          <th>船只物流信息</th>
-          <th>数值</th>
-        </tr>
-        <tr>
-          <td>经度</td>
-          <td>{{ position.lng}}</td>
-        </tr>
-        <tr>
-          <td>维度</td>
-          <td>{{position.lat}}</td>
-        </tr>
-        <tr>
-          <td>温度</td>
-          <td>{{temperature}} ℃</td>
-        </tr>
-        <tr>
-          <td>湿度</td>
-          <td>{{humidity}} %</td>
-        </tr>
-      </table>
-    </div>
-    <div v-show="carshow"
-         class="infocar">
-      <table border="1">
-        <tr>
-          <th>货车物流信息</th>
-          <th>数值</th>
-        </tr>
-        <tr>
-          <td>经度</td>
-          <td>{{ position.lng}}</td>
-        </tr>
-        <tr>
-          <td>维度</td>
-          <td>{{position.lat}}</td>
-        </tr>
-        <tr>
-          <td>状态</td>
-          <td>C语言之父</td>
-        </tr>
-        <tr>
-          <td>下一港口</td>
-          <td>C语言之父</td>
-        </tr>
-        <tr>
-          <td>港口延误时间</td>
-          <td>C语言之父</td>
-        </tr>
-      </table>
-    </div>
+    <p>当前时间：{{this.now}}<p>
+        <div v-show="shipshow"
+             class="info">
+          <table border="1">
+            <tr>
+              <th>船只物流信息</th>
+              <th>数值</th>
+            </tr>
+            <tr>
+              <td>经度</td>
+              <td>{{ position.lng}}</td>
+            </tr>
+            <tr>
+              <td>维度</td>
+              <td>{{position.lat}}</td>
+            </tr>
+            <tr>
+              <td>当前港口或下一港口</td>
+              <td>{{nextport}} </td>
+            </tr>
+            <tr>
+              <td>上一港口</td>
+              <td>{{preport}} </td>
+            </tr>
+            <tr>
+              <td>温度</td>
+              <td>{{temperature}} ℃</td>
+            </tr>
+            <tr>
+              <td>湿度</td>
+              <td>{{humidity}} %</td>
+            </tr>
+          </table>
+        </div>
+        <div v-show="carshow"
+             class="infocar">
+          <table border="1">
+            <tr>
+              <th>货车物流信息</th>
+              <th>数值</th>
+            </tr>
+            <tr>
+              <td>经度</td>
+              <td>{{ position.lng}}</td>
+            </tr>
+            <tr>
+              <td>维度</td>
+              <td>{{position.lat}}</td>
+            </tr>
+            <tr>
+              <td>状态</td>
+              <td>C语言之父</td>
+            </tr>
+            <tr>
+              <td>下一港口</td>
+              <td>C语言之父</td>
+            </tr>
+            <tr>
+              <td>港口延误时间</td>
+              <td>C语言之父</td>
+            </tr>
+          </table>
+        </div>
   </baidu-map>
 </template>
 
 <script>
 import { BmOverlay } from 'vue-baidu-map'
 import axios from 'axios'
+import { setInterval, clearInterval } from 'timers'
+
 export default {
   components: {
     BmOverlay
@@ -105,6 +114,13 @@ export default {
   },
   data () {
     return {
+      preport: '出发点',
+      nextport: '黄石',
+      now: new Date(),
+      nowtime: new Date().getTime(),
+      dtime: 0,
+      delay_time: 0,
+      total_delay: 0,
       carshow: false,
       shipshow: false,
       t1: new Date('2017-12-06 18:42:00'),
@@ -129,7 +145,10 @@ export default {
         delay_time: [
           { required: true, message: 'Please enter your user name', trigger: 'blur' }
         ]
-      }
+      },
+      portList: ['黄石', '武穴', '九江', '安庆', '池州', '铜陵', '芜湖', '马鞍山', '南京', '仪征', '镇江', '泰州', '常州', '江阴'],
+
+      portindex: [110, 159, 205, 378, 449, 495, 600, 669, 695, 715, 746, 824, 875, 922]
     }
   },
   // mounted() {
@@ -139,6 +158,10 @@ export default {
     await this.getPosition()
   },
   methods: {
+    handleCommand () {
+      this.delay_time = this.dtime
+      console.log(this.delay_time)
+    },
     handler ({ BMap, map }) {
       console.log(BMap, map)
       this.center.lng = 118.18214
@@ -167,11 +190,27 @@ export default {
         this.index += 1
         var t2 = new Date(m[2])
         this.position = { lng: m[0], lat: m[1] }
-        this.interval = (t2 - this.t1) / 24000000
+        var beishu = 2400
+        this.interval = (t2 - this.t1) / beishu
+        if (this.portindex.indexOf(this.index) !== -1) {
+          if (this.portindex.indexOf(this.index) === 0) {
+            this.preport = '出发点'
+            this.nextport = this.portList[0]
+          } else {
+            this.preport = this.portList[this.portindex.indexOf(this.index) - 1]
+            this.nextport = this.portList[this.portindex.indexOf(this.index)]
+          }
+          this.interval += this.delay_time * 3600000 / beishu
+          this.total_delay += this.delay_time
+          console.log('停留中')
+        }// 总延迟时间要写一下计算
+        this.t1 = t2
         var t = parseInt(Math.random() * 100)
         var h = parseInt(Math.random() * 500)
         this.temperature = 23 + t / 10
         this.humidity = 30 + h / 10
+        var asss = new Date(t2 - new Date('2017-12-06 18:42:00') + this.total_delay + this.nowtime)
+        this.now = asss
         console.log('lng:' + this.position.lng + ' ' + 'lat:' + this.position.lat)
       }
     },
@@ -237,6 +276,31 @@ export default {
   font-size: 16px;
 }
 .startProcess:focus {
+  background-color: red;
+}
+.delaytime {
+  position: absolute;
+  top: 30px;
+  left: 150px;
+  border: none;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+.submit {
+  position: absolute;
+  top: 30px;
+  left: 350px;
+  border: none;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+.submit:focus {
   background-color: red;
 }
 </style>
